2ff5a3c8849d567fcaf54d86e41877c8e82877a7
Reza Faraji <faraji.reza@outlook.com>
2022-07-14 22:42:36 +0430

    Setup the module structure and Migrated to gradle kotlin dsl

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,10 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val imageFinderData = ":image-finder:image_finder_data"
+    const val imageFinderDomain = ":image-finder:image_finder_domain"
+    const val imageFinderPresentation = ":image-finder:image_finder_presentation"
+}
---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,9 @@
+object ProjectConfig {
+    const val appId = "io.reza.paybackgroup"
+    const val compileSdk = 32
+    const val minSdk = 23
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val composeCompilerVersion = "1.2.0"
+}
